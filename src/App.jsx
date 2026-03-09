94da5651dc2861c5d073a758e3d8cda0e92ef83f
Shaurya Manhas <shauryamanhas64@gmail.com>
2026-03-09 17:25:27 +0530

    feat: add Lenis smooth scroll to whole website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Lenis from 'lenis';
 import Home from "./Home";
 import Contact from "./Contact";
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
   return (
     <Router>
       <Routes>
